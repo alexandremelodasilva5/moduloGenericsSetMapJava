76e3f20c47f5ff0f5bfbdf95c4c89c93e99e2c22
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-04 12:58:27 -0300

    testando igualdade no Set

--- appTestes/src/application/Program.java
+++ appTestes/src/application/Program.java
@@ -2,12 +2,17 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Circle;
 import entities.Rectangle;
 import entities.Shape;
 import entities.Clients;
+import entities.Product;
 
 public class Program {
 
@@ -47,6 +52,32 @@ public class Program {
 		Clients client3 = new Clients("Maira", "maira@gmail.com");
 		
 		
+		Set<String> set = new LinkedHashSet<>();
+		set.add("Tv");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		
+		Set<Integer> a = new TreeSet<Integer>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<Integer>(Arrays.asList(5,6,7,8,9,10));
+		
+		Set<Integer> c = new TreeSet<Integer>(a);
+		c.addAll(b);
+		
+		Set<Integer> d = new TreeSet<Integer>(a);
+		d.retainAll(b);
+		
+		Set<Integer> e = new TreeSet<Integer>(a);
+		e.removeAll(b);
+		
+		
+		Set<Product> setProduct = new HashSet<Product>();
+		setProduct.add(new Product("tv", 3000));
+		setProduct.add(new Product("note", 4000));
+		setProduct.add(new Product("tablet", 5000));
+		
+		
+		Product prod = new Product("note", 4000);
 		
 		System.out.println("A soma total das areas de shapes é : "+ String.format("%.2f", sum(list)));
 		System.out.println("A soma total das areas de circulos é: "+ String.format("%.2f", sum(listCircle)));
@@ -58,6 +89,17 @@ public class Program {
 		System.out.println(client1.equals(client2));
 		System.out.println(client2.equals(client3));
 		System.out.println(client2 == client3);
+		System.out.println(set.contains("Tablet"));
+		set.remove("Tv");
+		set.removeIf(x -> x.charAt(0) == 'N');
+		for(String s : set) {
+			System.out.print(s+" ");
+		}
+		System.out.println();
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(setProduct.contains(prod));
 		
 	}
 	
